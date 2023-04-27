231917da68f3047db41c7cf371104a25746d3243
Shoeb Shaikh <vc.shoeb.shaikh@zee.com>
2023-04-27 18:59:23 +0530

    wo as per fpc - MAM Work Design

--- lib/app/modules/MamWorkOrders/views/wo_as_per_daily_fpc_view.dart
+++ lib/app/modules/MamWorkOrders/views/wo_as_per_daily_fpc_view.dart
@@ -1,22 +1,65 @@
+import 'package:bms_scheduling/widgets/DateTime/DateWithThreeTextField.dart';
+import 'package:bms_scheduling/widgets/DateTime/TimeWithThreeTextField.dart';
+import 'package:bms_scheduling/widgets/FormButton.dart';
+import 'package:bms_scheduling/widgets/dropdown.dart';
+import 'package:bms_scheduling/widgets/gridFromMap.dart';
+import 'package:bms_scheduling/widgets/input_fields.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+
+import '../../RoBooking/views/dummydata.dart';
 
 class WoAsPerDailyFpcView extends GetView {
   const WoAsPerDailyFpcView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('WoAsPerDailyFpcView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'WoAsPerDailyFpcView is working',
-          style: TextStyle(fontSize: 20),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            DropDownField.formDropDown1WidthMap(
+                [], (value) => {}, "Work Order Type", 0.24),
+          ],
         ),
-      ),
+        Divider(
+          height: 10,
+        ),
+        Wrap(
+          alignment: WrapAlignment.start,
+          runAlignment: WrapAlignment.start,
+          crossAxisAlignment: WrapCrossAlignment.start,
+          spacing: Get.width * 0.005,
+          runSpacing: 5,
+          children: [
+            DropDownField.formDropDown1WidthMap(
+                [], (value) => {}, "Location", 0.12),
+            DropDownField.formDropDown1WidthMap(
+                [], (value) => {}, "Channel", 0.24),
+            DateWithThreeTextField(
+                title: "Ref Date",
+                widthRation: 0.12,
+                mainTextController: TextEditingController()),
+          ],
+        ),
+        Divider(
+          height: 10,
+        ),
+        Expanded(
+            child: Container(
+          color: Colors.amber,
+          child: DataGridFromMap(
+            mapData: dummyProgram,
+            formatDate: false,
+          ),
+        )),
+        SizedBox(
+          height: 5,
+        ),
+        FormButtonWrapper(btnText: "Save WO")
+      ],
     );
   }
 }
