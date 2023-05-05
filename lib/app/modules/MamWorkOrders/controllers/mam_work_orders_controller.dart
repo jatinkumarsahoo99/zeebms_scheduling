6fbbce60038e240b3426ee429e99b0eb6e97beb0
Nitish gupta <vc.nitish.gupta@zee.com>
2023-05-05 16:03:58 +0530

    mam work orders ui changes

--- lib/app/modules/MamWorkOrders/controllers/mam_work_orders_controller.dart
+++ lib/app/modules/MamWorkOrders/controllers/mam_work_orders_controller.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class MamWorkOrdersController extends GetxController {
-  //TODO: Implement MamWorkOrdersController
   List<String> mainTabs = ["Release WO Non FPC", "WO As Per Daily FPC", "WO Re-push", "Cancel WO", "WO History"];
   var selectedTab = "Release WO Non FPC".obs;
   PageController pageController = PageController();
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,19 +1,10 @@
 import 'package:get/get.dart';
 
 import '../modules/ImportDigitextRunOrder/bindings/import_digitext_run_order_binding.dart';
-import '../modules/ImportDigitextRunOrder/views/import_digitext_run_order_view.dart';
-import '../modules/FpcMismatch/bindings/fpc_mismatch_binding.dart';
-import '../modules/FpcMismatch/views/FpcMismatchView.dart';
-import '../modules/LogAdditions/bindings/log_additions_binding.dart';
-import '../modules/LogAdditions/views/LogAdditionsView.dart';
 import '../modules/MamWorkOrders/bindings/mam_work_orders_binding.dart';
 import '../modules/MamWorkOrders/views/mam_work_orders_view.dart';
 import '../modules/RoBooking/bindings/ro_booking_binding.dart';
 import '../modules/RoBooking/views/ro_booking_view.dart';
-import '../modules/SpotPriority/bindings/spot_priority_binding.dart';
-import '../modules/SpotPriority/views/SpotPriorityView.dart';
-import '../modules/TransmissionLog/bindings/transmission_log_binding.dart';
-import '../modules/TransmissionLog/views/TransmissionLogView.dart';
 import '../modules/commercial/bindings/commercial_binding.dart';
 import '../modules/commercial/views/commercial_view.dart';
 import '../modules/home/bindings/home_binding.dart';
