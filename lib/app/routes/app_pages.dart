c962555c2dd7d67a50a117ce2f6b3039227819d2
Nitish gupta <vc.nitish.gupta@zee.com>
2023-05-16 18:52:27 +0530

    MAM WO 1st tab api integrated

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -24,7 +24,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.RO_CANCELLATION +
+  static const INITIAL = Routes.MAM_WORK_ORDERS +
       "?personalNo=AqoF3cvt1PCPIKM8FfPwag%3D%3D&loginCode=gsS2oEkuYKzI9aXanDqobQ%3D%3D&formName=a4Lfy%2FGb5Roxo9vLiBCqSQ%3D%3D";
 
   static final routes = [
