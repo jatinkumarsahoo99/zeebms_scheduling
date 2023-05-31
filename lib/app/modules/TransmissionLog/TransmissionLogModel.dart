363b2098f65fbd405435701d7e978f89be5170ed
sanjayajena37 <sanjayajena00@gmail.com>
2023-05-31 23:46:13 +0530

    Added Changes for Transmission Log

--- lib/app/modules/TransmissionLog/TransmissionLogModel.dart
+++ lib/app/modules/TransmissionLog/TransmissionLogModel.dart
@@ -139,6 +139,7 @@ class LstTransmissionLog {
     data['breakNumber'] = this.breakNumber;
     data['episodeNumber'] = this.episodeNumber;
     data['breakEvent'] = this.breakEvent;
+    // data['rownumber'] = this.rownumber;
     data['rownumber'] = (this.rownumber!-1);
     data['eventType'] = this.eventType;
     data['bookingNumber'] = this.bookingNumber;
