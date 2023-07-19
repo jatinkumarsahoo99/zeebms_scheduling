1b31eccf62bf29dec67080cdc659024c40238a0b
Nitish <vc.nitish.gupta@zee.com>
2023-07-19 15:31:14 +0530

    inventory status report column width issue fixed

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -60,7 +60,7 @@ class AppPages {
   // static const INITIAL = Routes.ROS_DISTRIBUTION +
   static const INITIAL = kReleaseMode
       ? Routes.HOME
-      : Routes.MANAGE_CHANNEL_INVENTORY +
+      : Routes.INVENTORY_STATUS_REPORT +
           "?personalNo=kW5Bkf17%2FS5YF7ML28FmVg%3D%3D&loginCode=1BWIoBKeDl7qDSAAhxvXsQ%3D%3D&formName=OI8ukDpPPVN0I2BEXu2h4nuFu%2BZm1ZRpvP8NL4XCXzQ%3D";
 
   static final routes = [
