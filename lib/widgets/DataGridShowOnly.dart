d0c139fee5ad0ca498a23d1218831178ab4a9446
Nitish <vc.nitish.gupta@zee.com>
2023-08-03 15:34:49 +0530

    ro reschedule ui fixed

--- lib/widgets/DataGridShowOnly.dart
+++ lib/widgets/DataGridShowOnly.dart
@@ -97,9 +97,7 @@ class DataGridShowOnlyKeys extends StatelessWidget {
             enableContextMenu: false,
             width: 25,
             enableAutoEditing: false,
-            hide: hideCode! &&
-                key.toString().toLowerCase() != "hourcode" &&
-                key.toString().toLowerCase().contains("code"),
+            hide: hideCode! && key.toString().toLowerCase() != "hourcode" && key.toString().toLowerCase().contains("code"),
             enableColumnDrag: false,
             field: "no",
             type: PlutoColumnType.text()));
@@ -117,8 +115,7 @@ class DataGridShowOnlyKeys extends StatelessWidget {
                                 : key.toString().pascalCaseToNormal()
                         : key.toString().pascalCaseToNormal()
                     : key.toString(),
-                enableRowChecked:
-                    (checkRow == true && key == checkRowKey) ? true : false,
+                enableRowChecked: (checkRow == true && key == checkRowKey) ? true : false,
                 renderer: ((rendererContext) {
                   if (actionIconKey?.containsKey(key) ?? false) {
                     return GestureDetector(
@@ -148,12 +145,9 @@ class DataGridShowOnlyKeys extends StatelessWidget {
                   } else {
                     return GestureDetector(
                       onSecondaryTapDown: (detail) {
-                        rendererContext.stateManager.setCurrentCell(
-                            rendererContext.cell, rendererContext.rowIdx);
-                        DataGridMenu().showGridMenu(
-                            rendererContext.stateManager, detail, context,
-                            exportFileName: exportFileName,
-                            extraList: extraList);
+                        rendererContext.stateManager.setCurrentCell(rendererContext.cell, rendererContext.rowIdx);
+                        DataGridMenu()
+                            .showGridMenu(rendererContext.stateManager, detail, context, exportFileName: exportFileName, extraList: extraList);
                       },
                       child: Text(
                         (rendererContext.cell.value ?? "").toString(),
@@ -171,16 +165,11 @@ class DataGridShowOnlyKeys extends StatelessWidget {
                 enableContextMenu: false,
                 width: (keysWidths != null && keysWidths!.containsKey(key))
                     ? keysWidths![key]!
-                    : Utils.getColumnSize(
-                        key: key,
-                        value: mapData[0][key].toString(),
-                        widthRatio: widthRatio),
+                    : Utils.getColumnSize(key: key, value: mapData[0][key].toString(), widthRatio: widthRatio),
                 enableAutoEditing: false,
                 hide: showonly == null
                     ? (hideKeys != null && hideKeys!.contains(key)) ||
-                        hideCode! &&
-                            key.toString().toLowerCase() != "hourcode" &&
-                            key.toString().toLowerCase().contains("code")
+                        hideCode! && key.toString().toLowerCase() != "hourcode" && key.toString().toLowerCase().contains("code")
                     : !showonly!.contains(key),
                 enableColumnDrag: false,
                 field: key,
@@ -196,8 +185,7 @@ class DataGridShowOnlyKeys extends StatelessWidget {
                       ? "FPC Caption"
                       : key.toString().pascalCaseToNormal()
                   : key,
-              enableRowChecked:
-                  (checkRow == true && key == checkRowKey) ? true : false,
+              enableRowChecked: (checkRow == true && key == checkRowKey) ? true : false,
               renderer: ((rendererContext) {
                 if (actionIconKey?.containsKey(key) ?? false) {
                   if (actionIconKey?.containsKey(key) ?? false) {
@@ -214,20 +202,13 @@ class DataGridShowOnlyKeys extends StatelessWidget {
                   } else {
                     return GestureDetector(
                       onSecondaryTapDown: (detail) {
-                        rendererContext.stateManager.setCurrentCell(
-                            rendererContext.cell, rendererContext.rowIdx);
+                        rendererContext.stateManager.setCurrentCell(rendererContext.cell, rendererContext.rowIdx);
 
-                        DataGridMenu().showGridMenu(
-                            rendererContext.stateManager, detail, context,
-                            exportFileName: exportFileName,
-                            extraList: extraList);
+                        DataGridMenu()
+                            .showGridMenu(rendererContext.stateManager, detail, context, exportFileName: exportFileName, extraList: extraList);
                       },
                       child: Text(
-                        (checkRow == true &&
-                                key == checkRowKey &&
-                                hideCheckKeysValue)
-                            ? ""
-                            : rendererContext.cell.value.toString(),
+                        (checkRow == true && key == checkRowKey && hideCheckKeysValue) ? "" : rendererContext.cell.value.toString(),
                         style: TextStyle(
                           fontSize: SizeDefine.columnTitleFontSize,
                         ),
@@ -237,19 +218,13 @@ class DataGridShowOnlyKeys extends StatelessWidget {
                 } else {
                   return GestureDetector(
                     onSecondaryTapDown: (detail) {
-                      rendererContext.stateManager.setCurrentCell(
-                          rendererContext.cell, rendererContext.rowIdx);
+                      rendererContext.stateManager.setCurrentCell(rendererContext.cell, rendererContext.rowIdx);
 
-                      DataGridMenu().showGridMenu(
-                          rendererContext.stateManager, detail, context,
-                          exportFileName: exportFileName, extraList: extraList);
+                      DataGridMenu()
+                          .showGridMenu(rendererContext.stateManager, detail, context, exportFileName: exportFileName, extraList: extraList);
                     },
                     child: Text(
-                      (checkRow == true &&
-                              key == checkRowKey &&
-                              hideCheckKeysValue)
-                          ? ""
-                          : rendererContext.cell.value.toString(),
+                      (checkRow == true && key == checkRowKey && hideCheckKeysValue) ? "" : rendererContext.cell.value.toString(),
                       style: TextStyle(
                         fontSize: SizeDefine.columnTitleFontSize,
                       ),
@@ -262,15 +237,11 @@ class DataGridShowOnlyKeys extends StatelessWidget {
               enableEditingMode: editKeys != null && editKeys!.contains(key),
               enableDropToResize: true,
               enableContextMenu: false,
-              width: (keysWidths != null && keysWidths!.containsKey(key))
-                  ? keysWidths![key]!
-                  : Utils.getColumnSize(key: key, value: mapData[0][key]),
+              width: (keysWidths != null && keysWidths!.containsKey(key)) ? keysWidths![key]! : Utils.getColumnSize(key: key, value: mapData[0][key]),
               enableAutoEditing: false,
               hide: showonly == null
                   ? (hideKeys != null && hideKeys!.contains(key)) ||
-                      hideCode! &&
-                          key.toString().toLowerCase() != "hourcode" &&
-                          key.toString().toLowerCase().contains("code")
+                      hideCode! && key.toString().toLowerCase() != "hourcode" && key.toString().toLowerCase().contains("code")
                   : !showonly!.contains(key),
               enableColumnDrag: false,
               field: key,
@@ -290,10 +261,8 @@ class DataGridShowOnlyKeys extends StatelessWidget {
             cells[element.key] = PlutoCell(
               value: element.key == "selected" || element.value == null
                   ? ""
-                  : element.key.toString().toLowerCase().contains("date") &&
-                          formatDate!
-                      ? DateFormat(dateFromat).format(DateTime.parse(
-                          element.value.toString().replaceAll("T", " ")))
+                  : element.key.toString().toLowerCase().contains("date") && formatDate!
+                      ? DateFormat(dateFromat).format(DateTime.parse(element.value.toString().replaceAll("T", " ")))
                       : element.value.toString(),
             );
           }
@@ -323,9 +292,8 @@ class DataGridShowOnlyKeys extends StatelessWidget {
               onRowChecked: onRowChecked,
               rowColorCallback: colorCallback,
               onLoaded: (load) {
-                load.stateManager.setColumnSizeConfig(PlutoGridColumnSizeConfig(
-                    autoSizeMode: PlutoAutoSizeMode.none,
-                    resizeMode: PlutoResizeMode.normal));
+                load.stateManager
+                    .setColumnSizeConfig(PlutoGridColumnSizeConfig(autoSizeMode: PlutoAutoSizeMode.none, resizeMode: PlutoResizeMode.normal));
                 load.stateManager.setKeepFocus(false);
                 if (onload != null) {
                   onload!(load);
@@ -339,7 +307,7 @@ class DataGridShowOnlyKeys extends StatelessWidget {
       );
     } else {
       return Container(
-        decoration: BoxDecoration(border: Border.all(color: Colors.grey)),
+        decoration: BoxDecoration(border: Border.all(color: Colors.grey.shade100)),
       );
     }
   }
