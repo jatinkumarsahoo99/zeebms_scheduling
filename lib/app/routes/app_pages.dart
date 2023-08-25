b12650fad6b49511e15aab62495d5c81e94baf6d
Nitish Gupta <vc.nitish.gupta@zee.com>
2023-08-25 16:06:04 +0530

    common filter issue resolved

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -59,9 +59,7 @@ class AppPages {
   static const INITIAL = kReleaseMode
       ? Routes.HOME
       : Routes.ROS_DISTRIBUTION +
-          (1 == 1
-              ? "?personalNo=e7ScQlP7tXxBxRizIrkQIA%3D%3D&loginCode=scL134pM4e0EQtJCjv7r5Q%3D%3D"
-              : "?personalNo=kW5Bkf17%2FS5YF7ML28FmVg%3D%3D&loginCode=1BWIoBKeDl7qDSAAhxvXsQ%3D%3D&formName=OI8ukDpPPVN0I2BEXu2h4nuFu%2BZm1ZRpvP8NL4XCXzQ%3D");
+          "?personalNo=e7ScQlP7tXxBxRizIrkQIA%3D%3D&loginCode=scL134pM4e0EQtJCjv7r5Q%3D%3D&formName=MS0cvvaO5aFOBItqRvg6ulhDLdn7kvV8lN1s7pzGgWA%3D";
 
   static final routes = [
     GetPage(
