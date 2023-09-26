7202ab064bd6bea61f7e826c7860a690e99a1412
Nitish Gupta <vc.nitish.gupta@zee.com>
2023-09-26 16:58:04 +0530

    MAM save succes dialog message done

--- lib/app/modules/FillerMaster/model/dummy.dart
+++ lib/app/modules/FillerMaster/model/dummy.dart
@@ -2,7 +2,7 @@ import 'package:bms_scheduling/app/providers/Aes.dart';
 
 main() {
   // print(Uri.encodeComponent(Aes.encrypt('Release Order Booking').toString()));
-  print("Nitish User ID:${Uri.encodeComponent('scL134pM4e0EQtJCjv7r5Q==')}");
-  print(
-      "Nitish Personal No:${Uri.encodeComponent(Aes.encrypt("99990122") ?? '')}");
+  // print("Nitish User ID:${Uri.encodeComponent('scL134pM4e0EQtJCjv7r5Q==')}");
+  // print(
+  //     "Nitish Personal No:${Uri.encodeComponent(Aes.encrypt("99990122") ?? '')}");
 }
