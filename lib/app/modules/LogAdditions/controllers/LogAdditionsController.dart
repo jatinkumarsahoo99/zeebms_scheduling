4fb9e3a07271ec6a0d1b0b07b78bfaf92776f530
Nitish Gupta <vc.nitish.gupta@zee.com>
2023-10-05 12:45:51 +0530

    log addition focus issue resolved

--- lib/app/modules/LogAdditions/controllers/LogAdditionsController.dart
+++ lib/app/modules/LogAdditions/controllers/LogAdditionsController.dart
@@ -23,6 +23,11 @@ class LogAdditionsController extends GetxController {
   var channels = RxList<DropDownValue>([]);
   var additions = RxList<DropDownValue>([]);
   RxBool isEnable = RxBool(true);
+  var locationFN = FocusNode(),
+      primaryFN = FocusNode(),
+      secondaryFN = FocusNode(),
+      standbyLogFN = FocusNode(),
+      ignoreSportsFN = FocusNode();
 
   //input controllers
   DropDownValue? selectLocation;
@@ -269,7 +274,8 @@ class LogAdditionsController extends GetxController {
                     v["name"].toString().trim()) {
                   selectAdditions.value =
                       DropDownValue.fromJsonDynamic(v, "value", "name");
-                  selectAdditions1=DropDownValue.fromJsonDynamic(v, "value", "name");
+                  selectAdditions1 =
+                      DropDownValue.fromJsonDynamic(v, "value", "name");
                 }
               });
             }
