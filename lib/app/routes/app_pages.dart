1d385608c96856082bbae82dc07c628bbfc58ff5
jatinkumarsahoo <jatnkumarsahoo99@gmail.com>
2023-12-14 13:10:42 +0530

    brand master brand name uppercase bugs resolved

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -58,7 +58,7 @@ class AppPages {
 
   static const INITIAL = kReleaseMode
       ? Routes.HOME
-      : "${Routes.PROMO_MASTER}?personalNo=e7ScQlP7tXxBxRizIrkQIA%3D%3D&loginCode=scL134pM4e0EQtJCjv7r5Q%3D%3D&formName=ZdPOC%2BHl3hRJzHSclM%2B5m3sWVw1wpLPmNEvejYUOFLU%3D";
+      : "${Routes.BRAND_MASTER}?personalNo=e7ScQlP7tXxBxRizIrkQIA%3D%3D&loginCode=scL134pM4e0EQtJCjv7r5Q%3D%3D&formName=ZdPOC%2BHl3hRJzHSclM%2B5m3sWVw1wpLPmNEvejYUOFLU%3D";
 
   static final routes = [
     GetPage(
