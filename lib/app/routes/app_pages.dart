7c00741df75a5efa65fc180cca6f03921fada1dd
Mohd Maroof <vc.mohd.maroof@zee.com>
2024-03-05 10:22:02 +0530

    New File Updated.

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -58,7 +58,7 @@ class AppPages {
 
   static const INITIAL = kReleaseMode
       ? Routes.HOME
-      : "${Routes.SHORT_CONTENT_BULK_IMPORT}?personalNo=R9vVPL7er1Os%2FusemWG%2FIw%3D%3D&loginCode=0iGe3vK5h2KGjfSKZTpmsQ%3D%3D&formName=GFkgpt44E84UF3PtJkNRPw%3D%3D";
+      : "${Routes.RO_BOOKING}?personalNo=R9vVPL7er1Os%2FusemWG%2FIw%3D%3D&loginCode=0iGe3vK5h2KGjfSKZTpmsQ%3D%3D&formName=GFkgpt44E84UF3PtJkNRPw%3D%3D";
 
   static final routes = [
     GetPage(
